b0bd85d4d4b95a64a18431b9f9499fd9042433ea
杨子江 <yangzj@feiyu.com>
2022-11-07 17:19:43 +0800

    提交变更，方便被unity package系统集成
    可以方便直接用Unity Package的git导入，git地址为https://https://github.com/yangtzehina/ParticleEffectProfiler.git?path=/Assets/ParticleEffectProfiler

---
+++ Assets/ParticleEffectProfiler/Editor/TestParticleEffect.cs
@@ -0,0 +1,80 @@
+using ParticleEffectProfiler.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace ParticleEffectProfiler.Editor
+{
+    /// <summary>
+    /// 给选中的特效添加脚本的
+    /// </summary>
+    [InitializeOnLoad]
+    public static class TestParticleEffect
+    {
+        private const string RequestTestKey = "TestParticleEffectRquestTest";
+        private static bool _hasPlayed;
+        static bool isRestart = false;
+
+        [MenuItem("GameObject/特效/测试", false, 11)]
+        private static void Test()
+        {
+            var go = Selection.activeGameObject;
+            var particleSystemRenderer = go.GetComponentsInChildren<ParticleSystemRenderer>(true);
+
+            if (particleSystemRenderer.Length == 0)
+            {
+                Debug.LogError("不是特效无法测试！");
+                return;
+            }
+
+            EditorPrefs.SetBool(RequestTestKey, true);
+
+            //已经在播放状态，使其重新开始
+            if (EditorApplication.isPlaying)
+            {
+                EditorApplication.isPlaying = false;
+                isRestart = true;
+            }
+            else
+            {
+                EditorApplication.isPlaying = true;
+            }
+
+            var particleEffectScript = go.GetComponentsInChildren<ParticleEffectScript>(true);
+            if (particleEffectScript.Length == 0)
+            {
+                go.AddComponent<ParticleEffectScript>();
+            }
+        }
+
+        static TestParticleEffect()
+        {
+            EditorApplication.update += Update;
+            EditorApplication.playModeStateChanged += PlaymodeStateChanged;
+        }
+
+        private static void PlaymodeStateChanged(PlayModeStateChange obj)
+        {
+            if (!EditorApplication.isPlaying)
+            {
+                _hasPlayed = false;
+            }
+
+            if (isRestart)
+            {
+                EditorApplication.isPlaying = true;
+                isRestart = false;
+            }
+        }
+
+        private static void Update()
+        {
+            if (EditorPrefs.HasKey(RequestTestKey) && !_hasPlayed &&
+                EditorApplication.isPlaying &&
+                EditorApplication.isPlayingOrWillChangePlaymode)
+            {
+                EditorPrefs.DeleteKey(RequestTestKey);
+                _hasPlayed = true;
+            }
+        }
+    }
+}
